4ce8eaa8913b43ce74380c3b49655cf1a5dbe43b
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2025-04-22 14:55:31 -0300

    Mostrar estado de las operaciones concurrentes en consola para seguimiento y agregue Simulaciones para corroborar que funcioen devidamente

---
+++ src/modelo/RecursoConcreto.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class RecursoConcreto extends RecursoBase {
+
+    public RecursoConcreto(String identificador, String titulo, CategoriaRecurso categoria) {
+        super(identificador, titulo, categoria);
+    }
+
+}
+
+//SIRVE PARA PROBAR SIMULACIONES PORQUE NO PUEDO USAR RECURSOBASE AL SER ABSTRACTA ENTONCES CREE ESTA CLASE RECURSOCONCRETO//
